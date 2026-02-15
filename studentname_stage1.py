a09c3dd6a2a05c2779c362eaf66c22f6c0043e96
Rohan Periyasamy <81460031+RohanPeriyasamy@users.noreply.github.com>
2026-02-15 19:29:55 +0530

    Create studentname_stage1.py

---
+++ studentname_stage1.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+
+result = None
+
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    result = num1 / num2
+    
+print(result)
